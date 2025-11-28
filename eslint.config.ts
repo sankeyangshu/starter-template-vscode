17a2b2661cda985b62c6994acd2775466fb01b54
sankeyangshu <1260323835@qq.com>
2025-11-28 08:41:18 +0800

    chore: remove prettier config and update eslint settings

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,17 @@
-import { defineConfig } from '@sankeyangshu/eslint-config';
+import antfu from '@antfu/eslint-config';
 
-export default defineConfig();
+export default antfu({
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+  },
+  isInEditor: false,
+  rules: {
+    'style/arrow-parens': ['error', 'always'], // 箭头函数参数始终添加括号
+    'style/brace-style': ['error', '1tbs', { allowSingleLine: true }], // 括号样式
+  },
+});
